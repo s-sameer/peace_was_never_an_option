ec64ad17808a22e7a96549e07f3b288038b4b2c5
Karthik290902 <92994171+Karthik290902@users.noreply.github.com>
2023-03-25 15:13:56 -0230

    Ground_Collision
    Fixed the issue with ground collision - powerup

--- peace was never an option/Assets/PowerUpScript.cs
+++ peace was never an option/Assets/PowerUpScript.cs
@@ -7,11 +7,20 @@ public class PowerUpScript : MonoBehaviour
     public float moveSpeed = 10;
     public float deadZone = -12;
     public bool powerActivated = false;
-    
+    public float powerupDuration = 5f; // duration of the powerup effect in seconds
+
+    private Collider2D playerCollider; // store player's collider to toggle collision
+
+    private void Start()
+    {
+        playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        moveLeft();
+        if (!powerActivated) // move only if powerup is not active
+            moveLeft();
         remove();
     }
 
@@ -29,7 +38,6 @@ public class PowerUpScript : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        powerActivated = true;
         if (collision.CompareTag("Player"))
         {
             StartCoroutine(power(collision));
@@ -38,11 +46,24 @@ public class PowerUpScript : MonoBehaviour
 
     IEnumerator power(Collider2D player)
     {
-        player.GetComponent<BoxCollider2D>().enabled = false;
+        powerActivated = true;
+
+        // disable collision with obstacle layer
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Obstacle"), true);
+
         GetComponent<SpriteRenderer>().enabled = false;
-        yield return new WaitForSeconds(5);
-        player.GetComponent<BoxCollider2D>().enabled = true;
+        yield return new WaitForSeconds(powerupDuration);
+
+        // reset collision with obstacle layer
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Obstacle"), false);
+
+        powerActivated = false;
         Destroy(gameObject);
     }
 
+    private void OnDestroy()
+    {
+        // reset collision with obstacle layer when powerup is destroyed
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Obstacle"), false);
+    }
 }
