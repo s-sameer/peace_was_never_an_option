f7400f6a788646ecaea8f2935f4bcaf6cb511d48
annabehm <110179381+annabehm@users.noreply.github.com>
2023-04-03 14:21:47 -0230

    final shop implementation!!

--- peace was never an option/Assets/Pixel Adventure/More assets/Main Characters/ChangeGoose.cs
+++ peace was never an option/Assets/Pixel Adventure/More assets/Main Characters/ChangeGoose.cs
@@ -1,34 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class ChangeGoose : MonoBehaviour
 {
-    //public RuntimeAnimatorController gooseAnimatorController;
     public AnimatorOverrideController goose_1;
     public AnimatorOverrideController goose_2;
     public AnimatorOverrideController goose_3;
 
-//    void Start()
-//    {
-       // GetComponent<Animator>().runtimeAnimatorController = gooseAnimatorController;
-//    }
+    private AnimatorOverrideController currentGoose;
+
+    void Start()
+    {
+        int gooseIndex = PlayerPrefs.GetInt("gooseIndex", 1);
+        switch(gooseIndex)
+        {
+            case 1:
+                currentGoose = goose_1;
+                break;
+            case 2:
+                currentGoose = goose_2;
+                break;
+            case 3:
+                currentGoose = goose_3;
+                break;
+            default:
+                currentGoose = goose_1;
+                break;
+        }
+        GetComponent<Animator>().runtimeAnimatorController = currentGoose;
+    }
 
     public void Goose_1()
     {
-        GetComponent<Animator>().runtimeAnimatorController = goose_1 as RuntimeAnimatorController;
-        //goose_1.runtimeAnimatorController = gooseAnimatorController;
+        currentGoose = goose_1;
+        GetComponent<Animator>().runtimeAnimatorController = currentGoose;
+        PlayerPrefs.SetInt("gooseIndex", 1);
     }
 
     public void Goose_2()
     {
-        GetComponent<Animator>().runtimeAnimatorController = goose_2 as RuntimeAnimatorController;
-        //goose_2.runtimeAnimatorController = gooseAnimatorController;
+        currentGoose = goose_2;
+        GetComponent<Animator>().runtimeAnimatorController = currentGoose;
+        PlayerPrefs.SetInt("gooseIndex", 2);
     }
 
     public void Goose_3()
     {
-        GetComponent<Animator>().runtimeAnimatorController = goose_3 as RuntimeAnimatorController;
-        //goose_3.runtimeAnimatorController = gooseAnimatorController;
+        currentGoose = goose_3;
+        GetComponent<Animator>().runtimeAnimatorController = currentGoose;
+        PlayerPrefs.SetInt("gooseIndex", 3);
     }
 }
