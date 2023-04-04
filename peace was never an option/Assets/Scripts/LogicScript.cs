373854651f30f976cd7f35436cc6c84e46ac3252
Sadman Sameer <itsnotsameer@gmail.com>
2023-04-04 13:52:30 -0230

    added highest distance feature

--- peace was never an option/Assets/Scripts/LogicScript.cs
+++ peace was never an option/Assets/Scripts/LogicScript.cs
@@ -9,6 +9,7 @@ public class LogicScript : MonoBehaviour
     public Text scoreText;
     public Text coinText;
     public Text totalCoinText;
+    public Text highScore;
     public GameObject gameOverScreen;
     public float score = 0;
     public int coins = 0;
@@ -33,9 +34,8 @@ public class LogicScript : MonoBehaviour
     public void gameOver()
     {
         gameOverScreen.SetActive(true);
-        int totalCoins = PlayerPrefs.GetInt("coins");
-        PlayerPrefs.SetInt("coins", totalCoins + coins);
-        totalCoinText.text = PlayerPrefs.GetInt("coins").ToString();
+        updateTotalCoins();
+        updateHighScore();
     }
 
     public void MainMenu()
@@ -46,5 +46,22 @@ public class LogicScript : MonoBehaviour
     public void Shop()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void updateTotalCoins()
+    {
+        int totalCoins = PlayerPrefs.GetInt("coins");
+        PlayerPrefs.SetInt("coins", totalCoins + coins);
+        totalCoinText.text = PlayerPrefs.GetInt("coins").ToString();
+    }
+
+    public void updateHighScore()
+    {
+        int highscore = PlayerPrefs.GetInt("highscore");
+        if (score > highscore)
+        {
+            PlayerPrefs.SetInt("highscore", (int)score);
+        }
+        highScore.text = PlayerPrefs.GetInt("highscore").ToString();
     }
 }
